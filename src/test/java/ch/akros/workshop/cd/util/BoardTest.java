5a7ffb2f4cf84bd8a62c44325a4989de9644e5d6
adejung <andreas.dejung@akros.ch>
2014-04-20 11:16:29 +0200

    Place 1-5 implemented

--- src/test/java/ch/akros/workshop/cd/util/BoardTest.java
+++ src/test/java/ch/akros/workshop/cd/util/BoardTest.java
@@ -11,17 +11,17 @@ import org.junit.runner.RunWith;
 //@formatter:off
 /**
  * 
- * 1. Board has 5 places which can hold only stick (1-5) 
+ * 1. DONE Board has 5 places which can hold only stick (1-5) 
  * 2. Board has a bucket which has no limitation on how many stick it can hold (6) 
  * 3. DONE Done Try to put a stick on a specific number, if place is empty it returns 0, otherwise 2 sticks. 
  * 3a. DONE Put one in a empty Board shall return zero.
  * 3b. DONE Put a second one onto the same place return two.
- * 4. If place was not empty it is afterwards as the stick was returned to the player.
- * 4a Put 3rd times at the same space shall return 0.  
+ * 4. DONEvIf place was not empty it is afterwards as the stick was returned to the player.
+ * 4a DONE Put 3rd times at the same space shall return 0.  
  * 5. DONE list the state of the board. (1-5), do not show internal state therefore remove.
  * 5a. DONE list size is 5
  * 5b. DONE new Board all places are empty
- * 6. All places (1-5) shall behave the same
+ * 6. DONE All places (1-5) shall behave the same
  * 
  */
 //@formatter:on
@@ -34,28 +34,67 @@ public class BoardTest {
 	@Test
 	public void whenEmptyBoardAndPutInPlaceOneThenSticksReturnedZero() {
 		int place = 1;
-		int stickReturned = testee.put(place);
-
-		assertEquals("Returned sticks are not zero on place " + place, 0, stickReturned);
+		assertReturnZeroOnFirstPlacement(place);
 	}
 
 	@Test
 	public void whenEmptyBoardAndPutTwoInPlaceOneThenSticksReturnedTwo() {
 		int place = 1;
-		testee.put(place);
-		int stickReturned = testee.put(place);
-
-		assertEquals("Returned sticks are not two on place " + place, 2, stickReturned);
+		assertReturnTwoOnTwoPlacements(place);
 	}
 
 	@Test
 	public void whenEmtpyBoardAndPutThreeTimesInPlaceOneThenSticksReturnedZero() {
 		int place = 1;
-		testee.put(place);
-		testee.put(place);
-		int stickReturned = testee.put(place);
-
-		assertEquals("Returned sticks after putting 3 sticks are not zero on place " + place, 0, stickReturned);
+		assertReturnZeroOnThreePlacements(place);
 
 	}
+
+	@Test
+	public void ensureAllPlacesOneToFiveBehaveTheSameForFirstPlacement() {
+		for (int i = 1; i < 6; i++) {
+			assertReturnZeroOnFirstPlacement(i);
+		}
+	}
+
+	@Test
+	public void ensureAllPlacesOneToFiveBehaveTheSameForTwoPlacements() {
+		for (int i = 1; i < 6; i++) {
+			assertReturnTwoOnTwoPlacements(i);
+		}
+	}
+
+	@Test
+	public void ensureAllPlacesOneToFiveBehaveTheSameForThreePlacements() {
+		for (int i = 1; i < 6; i++) {
+			assertReturnZeroOnThreePlacements(i);
+		}
+	}
+
+	private int putAStick(int place) {
+		int stickReturned = testee.put(place);
+		return stickReturned;
+	}
+
+	private void assertReturnZeroOnFirstPlacement(int place) {
+		int stickReturned = putAStick(place);
+
+		assertEquals("Returned sticks are not zero on place " + place, 0, stickReturned);
+	}
+
+	private void assertReturnTwoOnTwoPlacements(int place) {
+		testee.put(place);
+		int stickReturned = putAStick(place);
+
+		assertEquals("Returned sticks are not two on place " + place, 2, stickReturned);
+	}
+
+	private void assertReturnZeroOnThreePlacements(int place) {
+		testee.put(place);
+		testee.put(place);
+		int stickReturned = putAStick(place);
+
+		assertEquals("Returned sticks after putting 3 sticks are not zero on place " + place, 0, stickReturned);
+	}
+
 }
