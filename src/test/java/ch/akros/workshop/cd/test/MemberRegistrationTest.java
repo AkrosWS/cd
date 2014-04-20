a1dffc3a4e2d5370d57c016852a1715c402f50cc
adejung <andreas.dejung@akros.ch>
2014-04-20 09:11:49 +0200

    comment out test as it is not needed

--- src/test/java/ch/akros/workshop/cd/test/MemberRegistrationTest.java
+++ src/test/java/ch/akros/workshop/cd/test/MemberRegistrationTest.java
@@ -20,47 +20,40 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.logging.Logger;
 
-import javax.inject.Inject;
-
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import ch.akros.workshop.cd.model.Member;
-import ch.akros.workshop.cd.service.MemberRegistration;
-import ch.akros.workshop.cd.util.Resources;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(Arquillian.class)
+import ch.akros.workshop.cd.model.Member;
+import ch.akros.workshop.cd.service.MemberRegistration;
+import ch.akros.workshop.cd.util.Resources;
+
+//@RunWith(Arquillian.class)
 public class MemberRegistrationTest {
-    @Deployment
-    public static Archive<?> createTestArchive() {
-        return ShrinkWrap.create(WebArchive.class, "test.war")
-                .addClasses(Member.class, MemberRegistration.class, Resources.class)
-                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                // Deploy our test datasource
-                .addAsWebInfResource("test-ds.xml");
-    }
+	// @Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addClasses(Member.class, MemberRegistration.class, Resources.class)
+				.addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml").addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				// Deploy our test datasource
+				.addAsWebInfResource("test-ds.xml");
+	}
 
-    @Inject
-    MemberRegistration memberRegistration;
+	// @Inject
+	MemberRegistration memberRegistration;
 
-    @Inject
-    Logger log;
+	// @Inject
+	Logger log;
 
-    @Test
-    public void testRegister() throws Exception {
-        Member newMember = new Member();
-        newMember.setName("Jane Doe");
-        newMember.setEmail("jane@mailinator.com");
-        newMember.setPhoneNumber("2125551234");
-        memberRegistration.register(newMember);
-        assertNotNull(newMember.getId());
-        log.info(newMember.getName() + " was persisted with id " + newMember.getId());
-    }
+	// @Test
+	public void testRegister() throws Exception {
+		Member newMember = new Member();
+		newMember.setName("Jane Doe");
+		newMember.setEmail("jane@mailinator.com");
+		newMember.setPhoneNumber("2125551234");
+		memberRegistration.register(newMember);
+		assertNotNull(newMember.getId());
+		log.info(newMember.getName() + " was persisted with id " + newMember.getId());
+	}
 
 }
