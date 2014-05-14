af770b93fe8c2dc523fba2a0b2dc254fc3c47c2c
adejung <andreas.dejung@akros.ch>
2014-05-14 19:46:01 +0200

    Scoreboard tested

--- src/test/java/ch/akros/workshop/cd/util/ScoreboardTest.java
+++ src/test/java/ch/akros/workshop/cd/util/ScoreboardTest.java
@@ -1,7 +1,7 @@
 package ch.akros.workshop.cd.util;
 
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.inOrder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.jglue.cdiunit.CdiRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -17,8 +18,8 @@ import ch.akros.workshop.cd.domain.Player;
 //@formatter:off
 /**
 * 
-* 1. SP1 has 10 Punkte , SP2,5 => log Scoreboard SP1, SP2
-* 2. SP1,10 , SP2,5 + new result SP1,1 , SP2,10 => log Scoreboard SP2,SP1
+* 1. DONE SP1 has 10 Punkte , SP2,5 => log Scoreboard SP1, SP2
+* 2. DONE SP1,10 , SP2,5 + new result SP1,1 , SP2,10 => log Scoreboard SP2,SP1
 * 
 */
 //@formatter:on
@@ -39,14 +40,34 @@ public class ScoreboardTest {
 
 	@Test
 	public void whenSP1Has10SP2Has5ThenLogSP1First() {
+		InOrder inOrder = inOrder(gameLogger);
+		Map<Player, Integer> scoreList = new HashMap<Player, Integer>();
+		scoreList.put(player2, 5);
+		scoreList.put(player1, 10);
+
+		testee.newScore(scoreList);
+		inOrder.verify(gameLogger).logScore(eq(player1), eq(10), eq(1));
+		inOrder.verify(gameLogger).logScore(eq(player2), eq(5), eq(1));
+	}
+
+	@Test
+	public void whenSP1Has10SP2Has5AndNewResultSP1Has1SP2Has10ThenLogSP2First() {
+		InOrder inOrder = inOrder(gameLogger);
 		Map<Player, Integer> scoreList = new HashMap<Player, Integer>();
 		scoreList.put(player2, 5);
 		scoreList.put(player1, 10);
 
 		testee.newScore(scoreList);
 
-		verify(gameLogger).logScore(eq(player1), eq(10), eq(1));
-		verify(gameLogger).logScore(eq(player2), eq(5), eq(1));
+		scoreList.put(player2, 10);
+		scoreList.put(player1, 1);
+
+		testee.newScore(scoreList);
+
+		inOrder.verify(gameLogger).logScore(eq(player1), eq(10), eq(1));
+		inOrder.verify(gameLogger).logScore(eq(player2), eq(5), eq(1));
+		inOrder.verify(gameLogger).logScore(eq(player2), eq(15), eq(2));
+		inOrder.verify(gameLogger).logScore(eq(player1), eq(11), eq(2));
 	}
 
 }
